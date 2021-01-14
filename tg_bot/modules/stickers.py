74c5edfd0a220b23c44556bdbbb99b9b89e5d97a
yupisky <74760335+yupisky@users.noreply.github.com>
2021-01-14 11:40:13 +0700

    Update stickers.py

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -237,7 +237,7 @@ __help__ = """
 __mod_name__ = "Stickers"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
-CURI_HANDLER = DisableAbleCommandHandler("curi", colong, pass_args=True, admin_ok=False)
+CURI_HANDLER = DisableAbleCommandHandler("colong", colong, pass_args=True, admin_ok=False)
 
 dispatcher.add_handler(STICKERID_HANDLER)
 dispatcher.add_handler(GETSTICKER_HANDLER)
